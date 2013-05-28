d6e3f4296179868dbf2a1f358bc37376a9bc5932
Mark Roddy <markroddy@gmail.com>
2013-05-28 14:48:43 -0400

    make curl not cause cron to flap due to writing to stderr

--- post-latest.sh
+++ post-latest.sh
@@ -14,7 +14,7 @@ THIS_FILE=`date --utc  +%F-%H-%M`.json
 
 
 echo "Downloading $URL_ENDPOINT to /tmp/$THIS_FILE"
-curl "$URL_ENDPOINT" > "/tmp/$THIS_FILE"
+curl --fail --silent "$URL_ENDPOINT" > "/tmp/$THIS_FILE"
 
 echo "S3 Putting /tmp/$THIS_FILE to $BUCKET_ROOT"
 s3cmd put "/tmp/$THIS_FILE" "$BUCKET_ROOT/$THIS_FILE"
